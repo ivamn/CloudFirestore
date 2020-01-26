650953a221e8e6aa637a16610954937cf7a212ef
Iván Gallego Torres <id.gallego16@iesdoctorbalmis.com>
2020-01-26 16:46:20 +0100

    Camvios

--- app/src/main/java/com/example/cloudfirestore/AddCiudad.java
+++ app/src/main/java/com/example/cloudfirestore/AddCiudad.java
@@ -1,13 +1,8 @@
 package com.example.cloudfirestore;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,17 +12,12 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.ByteArrayOutputStream;
-
-import static com.example.cloudfirestore.Util.BitMapToString;
-import static com.example.cloudfirestore.Util.bitmapFromUri;
-
 public class AddCiudad extends AppCompatActivity {
+    Ciudad c = new Ciudad();
     private EditText editCiudad, editPais;
     private ImageView imagen;
     private Button aceptar;
     private int COD_ELEGIR_IMAGEN = 1;
-    Ciudad c = new Ciudad();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +58,7 @@ public class AddCiudad extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == COD_ELEGIR_IMAGEN && resultCode == RESULT_OK) {
             Uri rutaImagen = data.getData();
-            c.setImagen(BitMapToString(bitmapFromUri(rutaImagen, getApplicationContext())));
-            imagen.setImageBitmap(bitmapFromUri(rutaImagen, getApplicationContext()));
+
         } else if (resultCode == RESULT_CANCELED) {
             Toast.makeText(this, "Se ha cancelado la operación", Toast.LENGTH_SHORT).show();
         }
--- app/src/main/java/com/example/cloudfirestore/EditCiudad.java
+++ app/src/main/java/com/example/cloudfirestore/EditCiudad.java
@@ -1,13 +1,8 @@
 package com.example.cloudfirestore;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,13 +12,9 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.TaskCompletionSource;
-
-import java.io.ByteArrayOutputStream;
-
 public class EditCiudad extends AppCompatActivity {
 
-    Ciudad c;
+    private Ciudad c;
     private EditText editCiudad, editPais;
     private ImageView imagen;
     private Button aceptar;
@@ -43,7 +34,7 @@ public class EditCiudad extends AppCompatActivity {
         aceptar = findViewById(R.id.buttonAceptar);
 
         if (c.getImagen() != null && !c.getImagen().equals("")) {
-            imagen.setImageBitmap(StringToBitMap(c.getImagen()));
+
         }
         editPais.setText(c.getPais());
         editCiudad.setText(c.getCiudad());
@@ -79,52 +70,9 @@ public class EditCiudad extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == COD_ELEGIR_IMAGEN && resultCode == RESULT_OK) {
             Uri rutaImagen = data.getData();
-            c.setImagen(BitMapToString(bitmapFromUri(rutaImagen, getApplicationContext())));
-            imagen.setImageBitmap(bitmapFromUri(rutaImagen, getApplicationContext()));
+
         } else if (resultCode == RESULT_CANCELED) {
             Toast.makeText(this, "Se ha cancelado la operación", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public Bitmap bitmapFromUri(Uri uri, Context context) {
-        ImageView imageViewTemp = new ImageView(context);
-        imageViewTemp.setImageURI(uri);
-        BitmapDrawable d = (BitmapDrawable) imageViewTemp.getDrawable();
-        return d.getBitmap();
-    }
-
-    public String BitMapToString(Bitmap bitmap) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 2, baos);
-        byte[] b = baos.toByteArray();
-        String temp = Base64.encodeToString(b, Base64.DEFAULT);
-        return temp;
-    }
-
-    public Bitmap convertirBytesBitmap(byte[] bytes) {
-        try {
-            return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-        } catch (Exception e) {
-            e.getMessage();
-            return null;
-        }
-    }
-
-    public byte[] convertirImagenBytes(Bitmap bitmap) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        return baos.toByteArray();
-    }
-
-
-    public Bitmap StringToBitMap(String encodedString){
-        try {
-            byte [] encodeByte=Base64.decode(encodedString,Base64.DEFAULT);
-            Bitmap bitmap=BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
-            return bitmap;
-        } catch(Exception e) {
-            e.getMessage();
-            return null;
         }
     }
 }
